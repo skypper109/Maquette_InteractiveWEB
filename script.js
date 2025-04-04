e1fbcb5fd7ee6abd04ff9b874f13149710207d8d
skypper109 <colonel.diallo19@gmail.com>
2025-04-04 07:58:48 +0000

    Commit de toutes les pages avec les fonctionnalités finies 04/04/2025

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ async function telechargerPDF() {
 
     // Récupère le bouton pour le cacher temporairement
     const bouton = document.getElementById('telecharger');
+    const couleur = document.getElementById('clr');
 
     // Cache le bouton avant la capture
     bouton.style.visibility = 'hidden';
+    couleur.style.visibility = 'hidden';
 
     // Capturer le reste de la page sans le bouton
     html2canvas(document.body).then(canvas => {
@@ -23,8 +25,16 @@ async function telechargerPDF() {
 
         // Affiche à nouveau le bouton après la capture
         bouton.style.visibility = 'visible';
+        couleur.style.visibility = 'visible';
 
         // Télécharge automatiquement le PDF nommé "page.pdf"
         pdf.save('cv.pdf');
     });
+}
+
+function changeBgCouleur(couleur){
+    // Change la couleur de la variable --bg-couleur dans la racine :root de la page
+    document.documentElement.style.setProperty('--bg-couleur', couleur);
+    // Change la couleur de fond du bouton
+    document.getElementById('telecharger').style.backgroundColor = couleur;
 }
